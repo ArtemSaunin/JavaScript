a1813b1ad0d214a0e48654d230bb072637094b0b
4sau <saynin86@mail.ru>
2016-03-15 16:34:21 +0200

    JS HT-1
    math.pow function

---
+++ js 1-2/js 1 - pow/js/script.js
@@ -0,0 +1,20 @@
+
+//var a = prompt('Введите число:');
+//var n = prompt('Введите степень:');
+
+function pow() {
+  var 
+  a = parseInt(document.form.a.value), //ввод данных через инпут
+  n = parseInt(document.form.n.value), //ввод данных через инпут
+  res = a;  
+    if (n==0) {
+        res = 1;
+    }else {
+    for (var i = 1; i < n; i++) {
+        res *= a;
+console.log('Результат: ', res);
+    }
+  }
+  return res;
+};
+alert(pow(a, n));

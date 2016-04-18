acbf45cfaeaf837c80a98b1b80c8fcaa0bfbebfb
ArtemSaunin <saunin86@gmail.com>
2016-04-18 16:41:40 +0300

    js 9-10

---
+++ js 9-10/js/jquery.checkbox.js
@@ -0,0 +1,33 @@
+'use strict'
+
+function changeCheck(el) {
+     var el = el;
+     var input = el.find("input").eq(0);
+   	 if(!input.attr("checked")) {
+		     el.css("background-position","-40px 0");
+         input.attr("checked", true)
+	} else {
+		el.css("background-position","0 0");
+		input.attr("checked", false)
+	}
+     return true;
+}
+
+function changeCheckStart(el) {
+  var el = el;
+	var input = el.find("input").eq(0);
+  if(input.attr("checked")) {
+		el.css("background-position","-40px 0");
+	}
+     return true;
+}
+
+$(function() {
+  $(".niceCheck").on('click', function() {
+    changeCheck($(this));
+  });
+  $(".niceCheck").each(function() {
+     changeCheckStart($(this));
+  });
+
+});
